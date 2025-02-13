d33d1ce99cead9a6b4bee99dd7f20c84198da5dc
Axelcruz2912 <al222310338@gmail.com>
2025-02-12 19:36:20 -0600

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+       HOLA MUNDOOOOOOOOOOOOOOOOO AXEL
         </p>
         <a
           className="App-link"
